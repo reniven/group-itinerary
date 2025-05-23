76b3e5b10ae837c492a77356ba9b425e0eb693c2
Eric Le <eric4leformal@gmail.com>
2025-05-23 10:17:53 -0500

    feat: implement signup form and update login page links

--- app/(auth-pages)/signup/page.tsx
+++ app/(auth-pages)/signup/page.tsx
@@ -1,13 +1,13 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
-import { LoginForm } from "@/components/forms/login-form"
+import { SignupForm } from "@/components/forms/signup-form"
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <a href="/" className="flex items-center gap-2 font-medium">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
@@ -16,7 +16,7 @@ export default function LoginPage() {
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-            <LoginForm />
+            <SignupForm />
           </div>
         </div>
       </div>
